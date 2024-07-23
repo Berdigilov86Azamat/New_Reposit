6e97cfc939433550ef75f89794607bafa0eb094a
Azamat Berdigulov <bazamat2009@mail.ru>
2024-07-23 07:57:54 +0600

    Добавил решения

---
+++ Task02/Program.cs
@@ -0,0 +1,33 @@
+﻿void Main()
+{
+    while (true)
+    {
+        Console.WriteLine("Введите целое число или 'q' для выхода:");
+        string input = Console.ReadLine()!;
+
+        if (input == "q")
+            break;
+
+        if (!int.TryParse(input, out int number))
+        {
+            Console.WriteLine("Введено некорректное значение. Попробуйте снова.");
+            continue;
+        }
+
+        int sum = 0;
+        foreach (char digit in Math.Abs(number).ToString())
+            sum += digit - '0';
+
+        if (sum % 2 == 0)
+        {
+            Console.WriteLine("Сумма цифр введенного числа чётная. Завершение программы.");
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Сумма цифр введенного числа нечетная. Продолжение программы.");
+        }
+    }
+}
+Main();
+
